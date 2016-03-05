3a44d6a97ce2ea389879eacc8011a0d9dde09ca4
Andy <php360@qq.com>
2016-03-05 17:38:03 +0800

    增加消息回复

---
+++ src/Msg.php
@@ -0,0 +1,15 @@
+<?php
+namespace Agile\Weixin;
+
+class Msg
+{
+    public static function requestMsg($postData)
+    {
+        return true;
+    }
+
+    public static function responseMsg()
+    {
+        //
+    }
+}
