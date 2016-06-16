06e72dff9c328994436622c9fa7ab653e51ce494
Andy <php360@qq.com>
2016-06-16 09:44:49 +0800

    e

--- src/Msg.php
+++ src/Msg.php
@@ -2,22 +2,22 @@
 /**
  * 微信消息被动回复
  *
- * @author    李扬(Andy) <php360@qq.com>
- * @link      技安后院 http://www.moqifei.com
+ * @author 李扬(Andy) <php360@qq.com>
+ * @link 技安后院 http://www.moqifei.com
  * @copyright 苏州幻果软件有限公司 http://www.huanguosoft.com
- * @license   http://www.apache.org/licenses/LICENSE-2.0
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 namespace Agile\Weixin;
 
 class Msg
 {
-    public static function requestMsg($postData)
+    public function requestMsg($postData)
     {
         //
         return true;
     }
 
-    public static function responseMsg()
+    public function responseMsg()
     {
         //
     }
